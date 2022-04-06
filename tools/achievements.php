2dfff47be155057c0152846c6a5d1ef71c90f329
lucaslking <lucasgiovannidesigner@gmail.com>
2022-04-06 17:11:31 -0300

    Added achievements page

---
+++ tools/achievements.php
@@ -0,0 +1,38 @@
+<?php
+$achievements = [
+	'1' => [
+		'name' => 'Allow Cookies?',
+		'grade' => '1',
+		'points' => '2',
+		'secret' => false,
+		'description' => 'With a perfectly harmless smile you fooled all of those wisecrackers into eating your exploding cookies. Consider a boy or girl scout outfit next time to make the trick even better.',
+	],
+	'2' => [
+		'name' => 'Allowance Collector',
+		'grade' => '1',
+		'points' => '2',
+		'secret' => true,
+		'description' => 'You certainly have your ways when it comes to acquiring money. Many of them are pink and paved with broken fragments of porcelain.',
+	],
+	'3' => [
+		'name' => 'Amateur Actor',
+		'grade' => '2',
+		'points' => '2',
+		'secret' => false,
+		'description' => 'You helped bringing Princess Buttercup, Doctor Dumbness and Lucky the Wonder Dog to life - and will probably dream of them tonight, since you memorised your lines perfectly. What a .. special piece of.. screenplay.',
+	],
+	'4' => [
+		'name' => 'Animal Activist',
+		'grade' => '3',
+		'points' => '2',
+		'secret' => false,
+		'description' => 'You have a soft spot for little, weak animals, and you do everything in your power to protect them - even if you probably eat dragons for breakfast.',
+	],
+	'5' => [
+		'name' => 'Annihilator',
+		'grade' => '3',
+		'points' => '5',
+		'secret' => false,
+		'description' => 'You\'ve daringly jumped into the infamous Annihilator and survived - taking home fame, glory and your reward.',
+	],
+];
