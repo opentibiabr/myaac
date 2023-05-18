6d10ed60ee89153740c604eaf28a0a4bcdea0899
Elson Costa <elsongabriel@hotmail.com>
2023-05-18 20:38:00 -0300

    migrate: Improved migrate functions and added clearCache function when migrate runs.

--- system/migrate.php
+++ system/migrate.php
@@ -2,21 +2,22 @@
 
 // database migrations
 $tmp = '';
-if(fetchDatabaseConfig('database_version', $tmp)) { // we got version
-	$tmp = (int)$tmp;
-	if($tmp < DATABASE_VERSION) { // import if older
-		$db->revalidateCache();
-		for($i = $tmp + 1; $i <= DATABASE_VERSION; $i++) {
-			require SYSTEM . 'migrations/' . $i . '.php';
-			updateDatabaseConfig('database_version', $i);
-		}
-	}
+if (fetchDatabaseConfig('database_version', $tmp)) { // we got version
+    $tmp = (int)$tmp;
+    if ($tmp < DATABASE_VERSION) { // import if older
+        runMigrate($tmp);
+    }
+} else { // register first version
+    registerDatabaseConfig('database_version', 0);
+    runMigrate();
 }
-else { // register first version
-	registerDatabaseConfig('database_version', 0);
-	$db->revalidateCache();
-	for($i = 1; $i <= DATABASE_VERSION; $i++) {
-		require SYSTEM . 'migrations/' . $i . '.php';
-		updateDatabaseConfig('database_version', $i);
-	}
+
+function runMigrate($tmp = 0)
+{
+    $db->revalidateCache();
+    for ($i = $tmp + 1; $i <= DATABASE_VERSION; $i++) {
+        require SYSTEM . 'migrations/' . $i . '.php';
+        updateDatabaseConfig('database_version', $i);
+        clearCache();
+    }
 }
