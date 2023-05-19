e3b4f5f561f2692b89924c1d92d46b85eae3321e
Elson Costa <elsongabriel@hotmail.com>
2023-05-19 10:24:39 -0300

    migrate: adjusts;

--- system/migrate.php
+++ system/migrate.php
@@ -1,27 +1,26 @@
 <?php
 
 // database migrations
-global $db;
 $tmp = '';
 if (fetchDatabaseConfig('database_version', $tmp)) {
     // we got version
     $tmp = (int)$tmp;
     if ($tmp < DATABASE_VERSION) {
+        $db->revalidateCache();
+        clearCache();
         // import if older
-        runMigrate($db, $tmp);
+        for ($i = $tmp + 1; $i <= DATABASE_VERSION; $i++) {
+            require SYSTEM . 'migrations/' . $i . '.php';
+            updateDatabaseConfig('database_version', $i);
+        }
     }
 } else {
     // register first version
     registerDatabaseConfig('database_version', 0);
-    runMigrate($db);
-}
-
-function runMigrate($db, $tmp = 0)
-{
     $db->revalidateCache();
-    for ($i = $tmp + 1; $i <= DATABASE_VERSION; $i++) {
+    clearCache();
+    for ($i = 1; $i <= DATABASE_VERSION; $i++) {
         require SYSTEM . 'migrations/' . $i . '.php';
         updateDatabaseConfig('database_version', $i);
-        clearCache();
     }
 }
