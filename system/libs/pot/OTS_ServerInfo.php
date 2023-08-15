deb9c8329a16ccbac383cb8ffb19dc6d558bb607
Elson Costa <elsongabriel@hotmail.com>
2023-08-15 11:12:41 -0300

    code formatting

--- system/libs/pot/OTS_ServerInfo.php
+++ system/libs/pot/OTS_ServerInfo.php
@@ -14,60 +14,59 @@
 
 /**
  * Various server status querying methods.
- * 
+ *
  * @package POT
  * @property-read OTS_InfoRespond|bool $status status() method wrapper.
  * @property-read OTS_ServerStatus|bool $info Full info() method wrapper.
  */
 class OTS_ServerInfo
 {
-/**
- * Server address.
- * 
- * @var string
- */
+    /**
+     * Server address.
+     *
+     * @var string
+     */
     private $server;
 
-/**
- * Connection port.
- * 
- * @var int
- */
+    /**
+     * Connection port.
+     *
+     * @var int
+     */
     private $port;
 
-/**
- * Creates handler for new server.
- * 
- * @param string $server Server IP/domain.
- * @param int $port OTServ port.
- */
+    /**
+     * Creates handler for new server.
+     *
+     * @param string $server Server IP/domain.
+     * @param int $port OTServ port.
+     */
     public function __construct($server, $port)
     {
         $this->server = $server;
         $this->port = $port;
     }
 
-/**
- * Sends packet to server.
- * 
- * @param OTS_Buffer|string $packet Buffer to send.
- * @return OTS_Buffer|null Respond buffer (null if server is offline).
- * @throws E_OTS_OutOfBuffer When there is read attemp after end of packet stream.
- */
+    /**
+     * Sends packet to server.
+     *
+     * @param OTS_Buffer|string $packet Buffer to send.
+     * @return OTS_Buffer|null Respond buffer (null if server is offline).
+     * @throws E_OTS_OutOfBuffer When there is read attemp after end of packet stream.
+     */
     private function send(OTS_Buffer $packet)
     {
         // connects to server
         $socket = @fsockopen($this->server, $this->port, $error, $message, config('status_timeout'));
 
         // if connected then checking statistics
-        if($socket)
-        {
+        if ($socket) {
             // sets 5 second timeout for reading and writing
             stream_set_timeout($socket, 5);
 
             // creates real packet
             $packet = $packet->getBuffer();
-            $packet = pack('v', strlen($packet) ) . $packet;
+            $packet = pack('v', strlen($packet)) . $packet;
 
             // sends packet with request
             // 06 - length of packet, 255, 255 is the comamnd identifier, 'info' is a request
@@ -75,16 +74,15 @@ class OTS_ServerInfo
 
             // reads respond
             //$data = stream_get_contents($socket);
-			$data = ''; 
-			while (!feof($socket))
-				$data .= fgets($socket, 1024);
+            $data = '';
+            while (!feof($socket))
+                $data .= fgets($socket, 1024);
 
             // closing connection to current server
             fclose($socket);
 
             // sometimes server returns empty info
-            if( empty($data) )
-            {
+            if (empty($data)) {
                 // returns offline state
                 return false;
             }
@@ -95,19 +93,19 @@ class OTS_ServerInfo
         return false;
     }
 
-/**
- * Queries server status.
- * 
- * <p>
- * Sends 'info' packet to OTS server and return output. Returns {@link OTS_InfoRespond OTS_InfoRespond} (wrapper for XML data) with results or <var>false</var> if server is online.
- * </p>
- * 
- * @return OTS_InfoRespond|bool Respond content document (false when server is offline).
- * @throws DOMException On DOM operation error.
- * @throws E_OTS_OutOfBuffer When there is read attemp after end of packet stream.
- * @example examples/info.php info.php
- * @tutorial POT/Server_status.pkg
- */
+    /**
+     * Queries server status.
+     *
+     * <p>
+     * Sends 'info' packet to OTS server and return output. Returns {@link OTS_InfoRespond OTS_InfoRespond} (wrapper for XML data) with results or <var>false</var> if server is online.
+     * </p>
+     *
+     * @return OTS_InfoRespond|bool Respond content document (false when server is offline).
+     * @throws DOMException On DOM operation error.
+     * @throws E_OTS_OutOfBuffer When there is read attemp after end of packet stream.
+     * @example examples/info.php info.php
+     * @tutorial POT/Server_status.pkg
+     */
     public function status()
     {
         // request packet
@@ -119,12 +117,11 @@ class OTS_ServerInfo
         $status = $this->send($request);
 
         // checks if server is online
-        if($status)
-        {
+        if ($status) {
             // loads respond XML
             $info = new OTS_InfoRespond();
-            if(!$info->loadXML( utf8_encode($status->getBuffer())))
-				return false;
+            if (!$info->loadXML(utf8_encode($status->getBuffer())))
+                return false;
 
             return $info;
         }
@@ -133,19 +130,19 @@ class OTS_ServerInfo
         return false;
     }
 
-/**
- * Queries server information.
- * 
- * <p>
- * This method uses binary info protocol. It provides more infromation then {@link OTS_Toolbox::serverStatus() XML way}.
- * </p>
- * 
- * @param int $flags Requested info flags.
- * @return OTS_ServerStatus|bool Respond content document (false when server is offline).
- * @throws E_OTS_OutOfBuffer When there is read attemp after end of packet stream.
- * @example examples/server.php info.php
- * @tutorial POT/Server_status.pkg
- */
+    /**
+     * Queries server information.
+     *
+     * <p>
+     * This method uses binary info protocol. It provides more infromation then {@link OTS_Toolbox::serverStatus() XML way}.
+     * </p>
+     *
+     * @param int $flags Requested info flags.
+     * @return OTS_ServerStatus|bool Respond content document (false when server is offline).
+     * @throws E_OTS_OutOfBuffer When there is read attemp after end of packet stream.
+     * @example examples/server.php info.php
+     * @tutorial POT/Server_status.pkg
+     */
     public function info($flags)
     {
         // request packet
@@ -157,8 +154,7 @@ class OTS_ServerInfo
         $status = $this->send($request);
 
         // checks if server is online
-        if($status)
-        {
+        if ($status) {
             // loads respond
             return new OTS_ServerStatus($status);
         }
@@ -167,19 +163,19 @@ class OTS_ServerInfo
         return false;
     }
 
-/**
- * Checks player online status.
- * 
- * <p>
- * This method uses binary info protocol.
- * </p>
- * 
- * @param string $name Player name.
- * @return bool True if player is online, false if player or server is online.
- * @throws E_OTS_OutOfBuffer When there is read attemp after end of packet stream.
- * @example examples/server.php info.php
- * @tutorial POT/Server_status.pkg
- */
+    /**
+     * Checks player online status.
+     *
+     * <p>
+     * This method uses binary info protocol.
+     * </p>
+     *
+     * @param string $name Player name.
+     * @return bool True if player is online, false if player or server is online.
+     * @throws E_OTS_OutOfBuffer When there is read attemp after end of packet stream.
+     * @example examples/server.php info.php
+     * @tutorial POT/Server_status.pkg
+     */
     public function playerStatus($name)
     {
         // request packet
@@ -192,28 +188,26 @@ class OTS_ServerInfo
         $status = $this->send($request);
 
         // checks if server is online
-        if($status)
-        {
+        if ($status) {
             $status->getChar();
-            return (bool) $status->getChar();
+            return (bool)$status->getChar();
         }
 
         // offline
         return false;
     }
 
-/**
- * Magic PHP5 method.
- * 
- * @param string $name Property name.
- * @param mixed $value Property value.
- * @throws OutOfBoundsException For non-supported properties.
- * @throws E_OTS_OutOfBuffer When there is read attemp after end of packet stream.
- */
+    /**
+     * Magic PHP5 method.
+     *
+     * @param string $name Property name.
+     * @param mixed $value Property value.
+     * @throws OutOfBoundsException For non-supported properties.
+     * @throws E_OTS_OutOfBuffer When there is read attemp after end of packet stream.
+     */
     public function __get($name)
     {
-        switch($name)
-        {
+        switch ($name) {
             case 'status':
                 return $this->status();
 
@@ -227,5 +221,3 @@ class OTS_ServerInfo
 }
 
 /**#@-*/
-
-?>
